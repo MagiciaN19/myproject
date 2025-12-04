719d0492f8c000578042c2ccd2c2e25e0795d4e8
Patryk <patryk.kucinski@student.ajp.edu.pl>
2025-12-04 16:36:55 +0100

    Lab 2

--- projekt_glowny/projekt_glowny/urls.py
+++ projekt_glowny/projekt_glowny/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("witaj.urls")),  
+    path('demo/', include('frontend_demo.urls')),
 ]
